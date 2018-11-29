d98ef0033640f673e3ae2bc7515122435c874ab6
Prakhar Saxena <prakhar@wego.com>
2018-11-29 16:37:40 +0800

    added solution of 14

---
+++ 14_LongestCollatzSequence.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def nextNum(x):
+	if (x % 2) == 0:
+		return (x/2)
+	else:
+		return ((3*x) +1)
+
+def chainLen(x):
+	next = nextNum(x)
+	if next == 1:
+		return 1
+	else:
+		return 1 + chainLen(next)
+
+maxLen = 1
+
+for i in range(1, 1000000):
+	cl = chainLen(i)
+	if cl > maxLen:
+		maxLen = cl
+
+print maxLen
