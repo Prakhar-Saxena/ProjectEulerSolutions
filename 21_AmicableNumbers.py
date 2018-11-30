b54778f52f737cd74af6dad6864bf0dc8a30cb2d
Prakhar Saxena <prakharsaxena914@gmail.com>
2018-11-30 11:57:21 +0800

    got the 21 solution right, i think

--- 21_AmicableNumbers.py
+++ 21_AmicableNumbers.py
@@ -7,12 +7,33 @@ def listOfDivisors(n):
 			l.append(i)
 	return l
 
+def sumOfList(x):
+        sum = 0
+        for i in x:
+                sum += i
+        return sum
+
 def d(n):
 	l = listOfDivisors(n)
-	return len(l)
+	return sum(l)
 
 def isAmicable(a,b):
-	if a != b and d(a) == b and d(b) == a:
+	if a != b  and d(b) == a: #not including d(a) == b, becasue that's a given whenever this function will be called
 		return True
 	else:
 		return False
+
+def listOfAmicableNumbers(n):
+	l = []
+	for a in range(1, n):
+		b = d(a)
+		if b < n and isAmicable(a, b):
+			l.append(a)
+			l.append(b)
+	return l
+
+l = listOfAmicableNumbers(10000)
+print l
+
+sum = sumOfList(l)
+print sum
