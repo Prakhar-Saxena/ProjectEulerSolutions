d9497166170e9c3387f4e4346bf6d61d80748b6b
Prakhar Saxena <prakhar@wego.com>
2018-11-30 11:11:23 +0800

    added 16, still thinking about 17

--- 16_PowerDigitSum.py
+++ 16_PowerDigitSum.py
@@ -4,4 +4,23 @@ num = 2 ** 1000
 
 def dig(x):
 	l = []
+	num = x
+	i = 0
+	while num != 0:
+		digit = num % 10
+		l.append(digit)
+		num /= 10
 	
+	return l
+
+def sumOfList(x):
+	sum = 0
+	for i in x:
+		sum += i
+	return sum
+
+l = dig(num)
+
+print num
+
+print sumOfList(l)
