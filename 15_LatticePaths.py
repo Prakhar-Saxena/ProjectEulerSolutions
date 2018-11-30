bcc9a15217771594edaba4be1319e1edffb9b945
Prakhar Saxena <prakhar@wego.com>
2018-11-30 10:05:03 +0800

    15 adn 16

---
+++ 15_LatticePaths.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#has to have 20 rights and 20 downs
+
+import math
+
+#now we have 2 values, so we're going to use factorials to arrange them
+
+boi = math.factorial(40) / ( ( math.factorial( 20 ) )^2 )
+
+print boi
---
+++ 16_PowerDigitSum.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+num = 2 ** 1000
+
+def dig(x):
+	l = []
+	
