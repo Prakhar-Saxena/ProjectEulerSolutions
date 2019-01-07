454e3096b174f1435346467da6e3e500e5f134f0
Prakhar Saxena <prakharsaxena914@gmail.com>
2019-01-07 09:14:26 +0800

    modified 30, don't remeber what though.. did it a long time ago..

--- 30_DigitFifthPowers.py
+++ 30_DigitFifthPowers.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
-ls[]
+ls=[]
 
 def getFifthSquareSum(x):
+	sum = 0
+	for i in x:
+		sum += (i^5)
+	return sum
 
 def getDigits(x):
-	
+	l = []
+	num = x
+	rem = 0
+	while num != 0:
+		rem = num % 10
+		num /= 10
+		l.append(rem)
+	return l
+
 
 x = 1
 
-while True:
-	
+print (9^5) * 5
+count = 1
+
+answerList = []
+while count <= ( ( 9 ^ 5 ) * 5):
+	if getFifthSquareSum(getDigits(count)) == count:
+		answerList.append(count)
+	count += 1
+
+
+print answerList
