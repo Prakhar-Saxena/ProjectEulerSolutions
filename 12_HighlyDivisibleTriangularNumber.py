b41da74228efb6faa1489570d6865318e1bc5313
Prakhar Saxena <prakharsaxena914@gmail.com>
2019-03-07 11:31:28 +0800

    made changes in 12, corrected 13

--- 12_HighlyDivisibleTriangularNumber.py
+++ 12_HighlyDivisibleTriangularNumber.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 def numOfDiv(x):
-	nod = 1
-	for i in range(2,x):
+	nod = 2
+	for i in range(2, int(x/2) + 1):
 		if (x % i) == 0:
 			nod += 1
 	return nod
 
 def sumOfNum(x):
 	sum = 0
-	for i in range(0,x+1):
+	for i in range(0, x + 1):
 		sum += i
 	return sum
 
@@ -17,7 +17,6 @@ i = 1
 while True:
 	n = sumOfNum(i)
 	nod = numOfDiv(n)
-	print n
 	if nod >= 500:
 		print "BOI", n
 		print i
