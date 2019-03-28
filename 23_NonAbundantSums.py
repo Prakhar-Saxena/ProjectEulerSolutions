f6308572ca5f766517939bfe8000febc8cc96e8d
Prakhar Saxena <prakharsaxena914@gmail.com>
2019-03-28 14:04:22 +0800

    still working on 23

--- 23_NonAbundantSums.py
+++ 23_NonAbundantSums.py
@@ -3,4 +3,19 @@
 def factors(x):
 	l = []
 	for i in range(1,x):
-		
+		if x % i == 0:
+			l.append(i)
+	return l
+
+def isPerfect(x):
+	fl = factors(x)
+	sumOfFact = sum(fl)
+	if sumOfFact == x:
+		return True
+	else:
+		return False
+
+def isDeficient(x):
+	fl = factors(x)
+	sumOfFace = sum(fl)
+	if sumOfFact == x
